46728fb5464793bf9f34e25124697815c86547e9
psivakoti <prasanth.sivakoti@cognizant.com>
2021-01-30 08:10:41 -0800

    Initial commit

---
+++ src/main/java/com/galvanize/musicshare/MusicshareApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.musicshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/musicshare/MusicshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.musicshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
