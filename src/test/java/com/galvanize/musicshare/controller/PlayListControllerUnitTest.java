0ea313f440ef0593ac75eb78e7ec6d05adb60177
Anurag Vishnoi <annu.vishnoi@gmail.com>
2021-01-30 13:39:51 -0500

    Created the add songs controller,unit test and service

---
+++ src/test/java/com/galvanize/musicshare/controller/PlayListControllerUnitTest.java
@@ -0,0 +1,39 @@
+package com.galvanize.musicshare.controller;
+
+import com.galvanize.musicshare.service.PlaylistService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class PlayListControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PlaylistService playlistService;
+
+    //put /playlist/{playlist_id}/song/{song_id} 201
+    @Test
+    public void addSongToPlayList() throws Exception {
+
+        when(playlistService.addSongToPlayList(anyString(), anyString())).thenReturn(1);
+
+        mockMvc.perform(put("/api/v1/playlist/{playlistName}/song/{songName}", "workout", "It is a good day"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(1));
+
+    }
+
+}
