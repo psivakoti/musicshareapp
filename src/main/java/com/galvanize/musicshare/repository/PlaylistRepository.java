3cdbe60d24a552951903f660c4ad3452006b96b7
psivakoti <prasanth.sivakoti@cognizant.com>
2021-01-30 13:11:47 -0800

    Resolving review comments

--- src/main/java/com/galvanize/musicshare/repository/PlaylistRepository.java
+++ src/main/java/com/galvanize/musicshare/repository/PlaylistRepository.java
@@ -2,6 +2,8 @@ package com.galvanize.musicshare.repository;
 
 import com.galvanize.musicshare.entity.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, String> {
 }
